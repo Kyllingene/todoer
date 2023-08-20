0101b45257e52219f7d0ed540adc157b1dce188a
Kyllingene <fuzzmuffin343@gmail.com>
2023-08-20 16:09:25 -0700

    Hide Invalid UTF-8 errors

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::process::exit;
+use std::{process::exit, io::ErrorKind};
 
 use ignore::WalkBuilder;
 use regex::Regex;
@@ -12,7 +12,6 @@ fn main() {
     parser.add(arg!(flag, both, 'i', "ignore"));
     parser.add(arg!(flag, both, 'H', "hidden"));
     parser.add(arg!(flag, both, 's', "short"));
-
     let remainder = match parser.parse() {
         Ok(r) => r,
         Err(e) => {
@@ -37,11 +36,15 @@ fn main() {
 
     let path = remainder.get(0).map(String::as_str).unwrap_or("./");
 
-	let short = get_flag!(parser, both, 's', "short");
-    let re = Regex::new("(#(\\[?)|//|;|--(\\[?)|/\\*+|\\{-|%(\\{?)|\\(\\*|<!--)+ *(BUG|HACK|TODO|FIXME|XXX) *:.*").unwrap();
+    let short = get_flag!(parser, both, 's', "short");
+    let re = Regex::new(
+        "(#(\\[?)|//|;|--(\\[?)|/\\*+|\\{-|%(\\{?)|\\(\\*|<!--)+ *(BUG|HACK|TODO|FIXME|XXX) *:.*",
+    )
+    .unwrap();
     for entry in WalkBuilder::new(path)
         .hidden(!get_flag!(parser, both, 'H', "hidden"))
-        .ignore(!get_flag!(parser, both, 'i', "ignore")).build()
+        .ignore(!get_flag!(parser, both, 'i', "ignore"))
+        .build()
     {
         match entry {
             Err(e) => {
@@ -59,23 +62,31 @@ fn main() {
 
                 match std::fs::read_to_string(entry.path()) {
                     Err(e) => {
-                        eprintln!("error (failed to read file): {e}");
+                        match e.kind() {
+                            ErrorKind::InvalidData => {},
+                            _ => eprintln!("error (failed to read file): {e}"),
+                        }
                     }
 
                     Ok(data) => {
                         for (li, line) in data.lines().enumerate() {
                             for m in re.find_iter(line) {
-                            	let name = if short {
-                            		entry.file_name().to_string_lossy()
-                            	} else {
-                            		entry.path().to_string_lossy()
-                            	};
-                                println!(" ({}:{}:{}) {}", name, li + 1, m.range().start + 1, m.as_str());
+                                let name = if short {
+                                    entry.file_name().to_string_lossy()
+                                } else {
+                                    entry.path().to_string_lossy()
+                                };
+                                println!(
+                                    " ({}:{}:{}) {}",
+                                    name,
+                                    li + 1,
+                                    m.range().start + 1,
+                                    m.as_str()
+                                );
                             }
                         }
                     }
                 }
-
             }
         }
     }
